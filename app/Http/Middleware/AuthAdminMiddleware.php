1add1178612558f6e39840e616ee2d1c21e6c098
vagrant <vagrant@homestead>
2018-09-17 10:05:09 +0000

    add notifications

--- app/Http/Middleware/AuthAdminMiddleware.php
+++ app/Http/Middleware/AuthAdminMiddleware.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
-class AdminAuthMiddleware
+class AuthAdminMiddleware
 {
     /**
      * Handle an incoming request.
